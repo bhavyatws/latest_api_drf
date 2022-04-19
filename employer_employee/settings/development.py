630e61ebfa454cc03caf1f0a7a77ea406f8e4377
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2022-04-19 16:58:19 +0530

    debug

--- employer_employee/settings/development.py
+++ employer_employee/settings/development.py
@@ -2,7 +2,7 @@ import os
 from employer_employee.settings.common import *
 
 
-DEBUG = True
+DEBUG = False
 
 SECRET_KEY = 'django-insecure-0a_e8kpav*e3fkv2rn9^vsh6p*n^b@=9yh4yprq&m!8a--z4p&'
 
--- employer_employee/settings/production.py
+++ employer_employee/settings/production.py
@@ -1,8 +1,8 @@
 import os
 from employer_employee.settings.common import *
  
-DEBUG = False
-DEBUG404=True
+DEBUG = True
+
 
 SECRET_KEY = 'django-insecure-0a_e8kpav*e3fkv2rn9^vsh6p*n^b@=9yh4yprq&m!8a--z4p&'
 
