43ce54b36b97389adf95920754fa8d21339ab9b3
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2022-04-20 11:43:07 +0530

    static_root changed to static_root

--- employer_employee/settings/common.py
+++ employer_employee/settings/common.py
@@ -131,7 +131,7 @@ MEDIA_DIR = BASE_DIR / 'media'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,  'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
