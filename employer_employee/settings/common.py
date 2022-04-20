865c7931c3d1aa52585c2da6abeddee8c07b4176
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2022-04-20 10:34:05 +0530

    changes in dev and prod

--- employer_employee/settings/common.py
+++ employer_employee/settings/common.py
@@ -135,14 +135,6 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-print(BASE_DIR)
-
-# Extra lookup directories for collectstatic to find static files
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- employer_employee/settings/development.py
+++ employer_employee/settings/development.py
@@ -3,6 +3,12 @@ from employer_employee.settings.common import *
 
 
 DEBUG = True
+if DEBUG:
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+       ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 SECRET_KEY = 'django-insecure-0a_e8kpav*e3fkv2rn9^vsh6p*n^b@=9yh4yprq&m!8a--z4p&'
 
