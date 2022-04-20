eb0f6d66c8d75bab6cc318badbed2ac9e8124eb3
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2022-04-20 10:00:37 +0530

    static folder moved to root project

--- employer_employee/settings/common.py
+++ employer_employee/settings/common.py
@@ -136,13 +136,12 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-print(BASE_DIR)
-
-# Extra lookup directories for collectstatic to find static files
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static')
 ]
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -167,7 +166,7 @@ django_heroku.settings(locals())
 
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 #Above STATICFILES_STORAGE does not allow to open admin page
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
