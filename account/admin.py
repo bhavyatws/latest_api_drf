0e3c26117ec5e47aab7d57cfc9bc9dd5dffe04ab
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2022-05-06 17:01:09 +0530

    modified code

--- account/admin.py
+++ account/admin.py
@@ -9,19 +9,30 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = User
-    list_display = ('email', 'is_staff', 'is_active', 'role', 'id')
-    list_filter = ('email', 'is_staff', 'is_active', 'role')
+    list_display = ("email", "is_staff", "is_active", "role", "id")
+    list_filter = ("email", "is_staff", "is_active", "role")
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'role')}),
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "role")}),
     )
     add_fieldsets = (
-        (None, {
-            'classes': ('wide', ),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'role')}),
-            )
-    search_fields = ('email', )
-    ordering = ('email', )
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "role",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
 
 
 admin.site.register(User, CustomUserAdmin)
